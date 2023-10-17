2eccd478e6a4ed65028ceb0d7c4f297595d75a48
tomasrobino <tomaserobino@gmail.com>
2023-10-17 17:26:49 +0200

    load bar done but with large size bug

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Loader from './Loader';
 import upload from "./upload-solid.svg"
 import { useRef, useState } from 'react';
 
 function App() {
+    const [loaderArr, setLoaderArr] = useState([]);
     const [files, setFiles] = useState([]);
     const ref = useRef()
 
@@ -15,22 +17,40 @@ function App() {
             //console.log(e.target.files)
             let selectedFiles = Object.values(e.target.files);
             selectedFiles.forEach(element => {
-                if (files.includes(element)) {
-                    console.log("already selected")
-                } else {
+                if (!files.includes(element)) {
                     setFiles([...files, element]);
-                    selectedFiles.push(element)
-                    console.log("added");
+                    /*
+                    let reader = new FileReader();
+                    reader.readAsDataURL(element);
+                    reader.addEventListener("progress", e => {
+                        console.log("aaa")
+                        //setProgress( Math.round( (e.loaded/e.total)*100 ) );
+                    });
+                    */
+                    //loaderArr.push(<Loader element={element}/>);
+                    setLoaderArr([...loaderArr, <Loader element={element}/>]);
                 }
             });
         }
     }
+
+
+    /*
+    const [progress, setProgress] = useState(0);
+    let reader = new FileReader();
+    reader.readAsDataURL(props.element);
+    reader.addEventListener("progress", e => {
+        console.log(e)
+        setProgress( Math.round( (e.loaded/e.total)*100 ) );
+    });
+    */
 
     return (
         <div className="App">
             <div className='div'>
                 <img alt='Upload' src={upload} onClick={handleClick} className='uploadIcon'/>
                 <input ref={ref} type="file" hidden onChange={handleSelection} multiple/>
+                {loaderArr}
             </div>
         </div>
     );
---
+++ src/Loader.js
@@ -0,0 +1,44 @@
+import { Box, CircularProgress, LinearProgress, Typography } from "@mui/material";
+import { useState } from "react"
+import check from "./check-solid.svg"
+
+export default function Loader(props) {
+    const [progress, setProgress] = useState(0);
+    let reader = new FileReader();
+    reader.readAsDataURL(props.element);
+    reader.addEventListener("loadstart", e => {
+        
+    })
+    reader.addEventListener("progress", e => {
+        console.log(e.loaded)
+        setProgress( Math.round( (e.loaded/e.total)*100 ) );
+    });
+    reader.addEventListener("load", e => {
+
+    })
+
+    function LinearProgressWithLabel(props) {
+        return (
+            <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Box sx={{ minWidth: 35, marginRight: "10px" }}>
+                    <Typography variant="body2" color="text.secondary">{`${Math.round(
+                        props.value,
+                    )}%`}</Typography>
+                </Box>
+                <Box sx={{ width: '100%', mr: 1 }}>
+                    <LinearProgress variant="determinate" {...props} sx={{width: "70vw"}}/>
+                </Box>
+            </Box>
+        );
+    }
+
+    return(
+        <div className="loaderDiv">
+            {progress===100? <img alt="" src={check} className="check"/> :<CircularProgress/>}
+            <div className="barDiv">
+                <LinearProgressWithLabel value={progress}/>
+            </div>
+            <img src={URL.createObjectURL(props.element)} alt="thumbnail" className="thumbnail"/>
+        </div>
+    )
+}
